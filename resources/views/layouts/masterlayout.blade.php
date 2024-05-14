d41815201db9d219eba044282a4c8d25c92f8799
Ali <rayangosling795@gmail.com>
2024-05-15 02:38:48 +0500

    first commit

---
+++ resources/views/layouts/masterlayout.blade.php
@@ -0,0 +1,7 @@
+@include('includes.navbar')
+
+<!-- Content -->
+@yield('content')
+<!-- / Content -->
+
+@include('includes.footer')
---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('pages/dashboard');
+});
+
+Route::get('/login', function () {
+    return view('pages/login');
+});
+
+Route::get('/register', function () {
+    return view('pages/register');
+});
